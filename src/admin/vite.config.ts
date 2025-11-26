37ee17d01456ef046c29913e714767245acab23d
Christian Meichtry <chris@atelier-agile.ch>
2025-11-26 10:00:04 +0100

    hosts

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -3,6 +3,9 @@ import { mergeConfig, type UserConfig } from 'vite';
 export default (config: UserConfig) => {
   // Important: always return the modified config
   return mergeConfig(config, {
+    server: {
+        allowedHosts: true,
+    },
     resolve: {
       alias: {
         '@': '/src',
